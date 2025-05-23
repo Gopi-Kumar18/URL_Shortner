14215517a55b505b469ac15704dad6a98e3566e6
gk811 <gptz1811@gmail.com>
2025-05-23 16:53:13 +0530

    ignore .env file and node_modules/

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 
 const express = require("express");
 const path = require("path");
+
 const cors = require("cors");
 const { connectToMongo } = require("./connection");
+
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
-const shareRoute = require("./routes/share");
+const fetchShortID = require("./models/fetchShortID");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-// Connect to MongoDB
-connectToMongo('mongodb://shortner:shortUrl.shortner%26%3F@localhost:27017/short-url?authSource=admin')
+
+connectToMongo(process.env.MONGODB_URI)
     .then(() => {
         console.log("MongoDB connected Successfully...");
     })
@@ -28,45 +34,17 @@ app.use('/styles', express.static(path.join(__dirname,'styles')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Route to render landing page
+// This will render views/index.ejs
 app.get('/', (req, res) => {
-    res.render('index'); // This will render views/index.ejs
+    res.render('index'); 
 });
+
+app.get('/:shortId',fetchShortID);
+
 
 // API route for URL shortening
 app.use("/url", urlRoute);
 
-
-// Redirect route for shortened URLs
-app.get('/:shortId', async (req, res) => {
-    const shortId = req.params.shortId;
-    console.log(`Received shortId: ${shortId}`);
-
-    try {
-        const entry = await URL.findOneAndUpdate(
-            { shortId },
-            {
-                $push: {
-                    visitHistory: { timestamp: Date.now() },
-                },
-            },
-            { new: true }
-        );
-
-        console.log('Database entry:', entry);
-
-        if (!entry) {
-            return res.status(404).send('URL not found');
-        }
-
-        res.redirect(entry.redirectURL);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ models/fetchShortID.js
@@ -0,0 +1,29 @@
+const fetchShortID = async (req, res) => {
+    const shortId = req.params.shortId;
+    console.log(`Received shortId: ${shortId}`);
+
+    try {
+        const entry = await URL.findOneAndUpdate(
+            { shortId },
+            {
+                $push: {
+                    visitHistory: { timestamp: Date.now() },
+                },
+            },
+            { new: true }
+        );
+
+        console.log('Database entry:', entry);
+
+        if (!entry) {
+            return res.status(404).send('URL not found');
+        }
+
+        res.redirect(entry.redirectURL);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+};
+
+module.exports = fetchShortID;
